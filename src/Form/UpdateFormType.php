1e6b65b0dcf8fbe6c3d73f98304530bdd9e27041
eyaa <eya.hammar@insat.ucar.tn>
2024-05-10 12:03:19 +0100

    update

--- src/Form/UpdateFormType.php
+++ src/Form/UpdateFormType.php
@@ -6,17 +6,19 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class UpdateFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('user_type')
-            ->add('job')
-            ->add('city')
-            ->add('image_url')
-            ->add('personal_info')
+            ->add('user_type',TextType::class)
+            ->add('job',TextType::class)
+            ->add('city',TextType::class)
+            ->add('image_url',FileType::class)
+            ->add('personal_info',TextType::class)
         ;
     }
 
