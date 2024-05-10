a28b33daee906eb97a7d0153208d2fd4f4801fd1
eyaa <eya.hammar@insat.ucar.tn>
2024-05-10 10:40:56 +0100

    update

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,13 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'profile')]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        $user = $this->getUser();
         return $this->render('profile/index.html.twig', [
+            'user'=>$user,
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    #[Route('/profile/update', name: 'profileupdate')]
+    public function update(): Response
+    {
+        return $this->render('profile/update.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
     }
