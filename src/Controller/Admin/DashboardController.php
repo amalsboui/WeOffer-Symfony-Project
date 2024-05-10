2b5201700075466104d08049be8cf2a41813136d
amalsboui <amal.sboui@insat.ucar.tn>
2024-05-10 14:20:09 +0100

    login

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Application;
+use App\Entity\Job;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -10,11 +13,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         //return parent::index();
+        /*$userRepository = $this->entityManager->getRepository(User::class);
+        $counts = $userRepository->countUsersByType();
+        $totalUsers = $counts[0]['totalUsers'];
+        $totalJobSeekers = $counts[0]['totalJobSeekers'];
+        $totalRecruiters = $counts[0]['totalRecruiters'];
 
+        return $this->render('home_admin/index.html.twig',[
+            'totalUsers' => $totalUsers,
+            'totalJobSeekers' => $totalJobSeekers,
+            'totalRecruiters' => $totalRecruiters,
+        ]);*/
 
          return $this->render('admin/home.html.twig');
     }
@@ -22,12 +36,17 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Symfony Project');
+            ->setTitle('Weoffer Admin Dashboard');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [
+            MenuItem::linkToDashboard('Home', 'fa fa-home'),
+            MenuItem::linkToCrud('Users', 'fas fa-users', User::class),
+            MenuItem::linkToCrud('Jobs', 'fas fa-briefcase', Job::class),
+            MenuItem::linkToCrud('Applications', 'fas fa-file', Application::class),
+
+        ];
     }
 }
